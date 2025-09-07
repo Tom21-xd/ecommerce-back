56db325abe7cd835052802d1d613e4fe6e93ff41
Johan Steven Ramirez Murcia <johan05182002.com@gmail.com>
2025-09-06 22:36:10 -0500

    fix limite de json

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Marketplace')
-    .setDescription('Proyecto de Ecommerce encaminado a la asignatura de Desarrollo de Redes II')
+    .setDescription(
+      'Proyecto de Ecommerce encaminado a la asignatura de Desarrollo de Redes II',
+    )
     .setVersion('1.0')
     .addBearerAuth()
     .build();
@@ -18,6 +21,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   app.enableCors();
+  app.use(json({ limit: '15mb' }));
+  app.use(urlencoded({ extended: true, limit: '15mb' }));
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
